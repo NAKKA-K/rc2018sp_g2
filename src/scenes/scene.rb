434ed6102616cae083713630a2c9fb4cbf283f48
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-03-06 10:56:35 +0900

    Add Sceneの親クラスを簡易作成

--- src/scenes/scene.rb
+++ src/scenes/scene.rb
@@ -1,2 +1,20 @@
 class Scene
+    def initialize(board)
+        @font = Font.new(32, 'MS Pゴシック')
+        @board = nil
+    end
+
+    def play
+        raise NotImplementedError
+    end
+
+    private
+    
+    def update
+        raise NotImplementedError
+    end
+
+    def draw
+        raise NotImplementedError
+    end
 end
