5ec8eb979fbf8f785a2943b8bef555e8a88ae831
t-kazu <s153057@matsu.shimane-u.ac.jp>
2018-03-07 22:59:37 +0900

    音楽を流してみた

--- src/main.rb
+++ src/main.rb
@@ -1,5 +1,6 @@
 require 'dxruby'
 require_relative './config'
+require_relative './music'
 require_relative 'scene_mgr'
 require_relative 'scenes/title/director'
 require_relative 'scenes/game/director'
@@ -19,7 +20,11 @@ SceneMgr.add(Credit::Director.new(), :credit)
 
 SceneMgr.move_to(:title)
 
+count = 0
+
 Window.loop do
     break if Input.key_push?(K_ESCAPE)
+    $correct.play if count % 120 == 0
     SceneMgr.play
+    count += 1
 end
---
+++ src/music.rb
@@ -0,0 +1,4 @@
+require 'dxruby'
+require_relative './config'
+
+$correct = Sound.new("#{$ROOT_PATH}/sound/correct.wav")
