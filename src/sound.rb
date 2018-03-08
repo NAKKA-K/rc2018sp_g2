12de3c06c28c7c0a7ef6140612dcf339316ea71a
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-03-08 11:36:09 +0900

    Add サウンド切り替えを呼び出し元に意識させないように、2つ実装

--- src/sound.rb
+++ src/sound.rb
@@ -2,9 +2,14 @@ require 'dxruby'
 require_relative './config'
 
 class Sound
-    @@correct_sound = Sound.new("#{$ROOT_PATH}/sound/correct.wav")
+    @@correct_effect_sound = Sound.new("#{$ROOT_PATH}/sound/correct.wav")
+    @@wrong_effect_sound = Sound.new("#{$ROOT_PATH}/sound/correct.wav")
 
     def self.correct_effect_play
-        @@correct_sound.play
+        @@correct_effect_sound.play
+    end
+
+    def self.wrong_effect_play
+        @@wrong_effect_sound.play
     end
 end
