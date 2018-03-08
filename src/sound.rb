1efd9c8d020227ae59700944ceda3f541892f2ea
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-03-08 11:30:36 +0900

    Update グローバル変数撲滅委員会による粛清

--- src/sound.rb
+++ src/sound.rb
@@ -1,4 +1,10 @@
 require 'dxruby'
 require_relative './config'
 
-$correct_sound = Sound.new("#{$ROOT_PATH}/sound/correct.wav")
+class Sound
+    @@correct_sound = Sound.new("#{$ROOT_PATH}/sound/correct.wav")
+
+    def self.correct_effect_play
+        @@correct_sound.play
+    end
+end
